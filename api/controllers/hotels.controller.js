6d971c173bf083932cd89ee50977046beda2924b
Dhindsa91 <arvind.dhindsa1@gmail.com>
2017-12-17 17:54:28 -0800

    controllers added

---
+++ api/controllers/hotels.controller.js
@@ -0,0 +1,15 @@
+var hotelData = require('../data/hotel-data.json');
+
+module.exports.hotelsGetAll = function(req, res) {
+  console.log('Get the hotels');
+
+  res.status(200).json(hotelData);
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+  var hotelId = req.params.hotelId;
+  var thisHotel = hotelData[hotelId];
+  console.log('Get the hotels', hotelId);
+
+  res.status(200).json(thisHotel);
+};
--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var routes = require('./api/routes');
 
 app.set('port', 3000);
 
+app.use('/css', function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', routes);
 
 app.get('/json', function(req, res) {
   console.log('Get the json');
