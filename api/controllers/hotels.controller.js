916e0d6dcf26eb25b5eba1e92090f99f0a33a13c
Dhindsa91 <arvind.dhindsa1@gmail.com>
2017-12-19 16:33:20 -0800

    added controllers and mongodb

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -1,15 +1,44 @@
+var dbconn = require('../data/connection.js');
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res) {
-  console.log('Get the hotels');
+  var db = dbconn.get();
+  var collection = db.collection('hotels');
 
-  res.status(200).json(hotelData);
+  collection
+    .find()
+    .skip(offset)
+    .limit(count)
+    .toArray(function(err, docs) {
+      console.log('found hotels', docs);
+      res.status(200).json(docs);
+    });
+
+  // console.log('db', db);
+  // console.log('Get the hotels');
+  // console.log(req.query);
+
+  var offset = 0;
+  var count = 5;
+
+  if (req.query && req.query.offset) {
+    offset = parseInt(req.query.offset, 10);
+  }
+  if (req.query && req.query.count) {
+    count = parseInt(req.query.count, 10);
+  }
+  var returnData = hotelData.slice(offset, offset + count);
+
+  res.status(200).json(returnData);
+};
+
+module.exports.hotelsAddOne = function(req, res) {
+  console.log('post new hotel');
+  console(req.body);
+  res.status(200).json(req.body);
 };
 
 module.exports.hotelsGetOne = function(req, res) {
-  var hotelId = req.params.hotelId;
-  var thisHotel = hotelData[hotelId];
-  console.log('Get the hotels', hotelId);
-
-  res.status(200).json(thisHotel);
+  console.log('GET hotelId', req.params.hotelId);
+  res.status(200).json(hotelData[req.params.hotelId]);
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlHotels = require('../controllers/hotels.controller.js');
+
 router.route('/hotels').get(ctrlHotels.hotelsGetAll);
 
-router.route('/hotels:hotelId').get(ctrlHotels.hotelsGetOne);
+router.route('/hotels/:hotelId').get(ctrlHotels.hotelsGetOne);
 
+router.route('/hotels/new').post(ctrlHotels.hotelsAddOne);
 module.exports = router;
